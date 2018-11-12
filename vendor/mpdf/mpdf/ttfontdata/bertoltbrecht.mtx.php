2d77e57d95d59d9349d5ad0d62c3d56fcd8f261f
Ahwidemedia <44965367+Ahwidemedia@users.noreply.github.com>
2018-11-12 10:53:59 +0100

    Add files via upload

---
+++ vendor/mpdf/mpdf/ttfontdata/bertoltbrecht.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='BertoltBrecht';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 714,
+  'XHeight' => 476,
+  'FontBBox' => '[-55 -262 945 910]',
+  'Flags' => 4,
+  'Ascent' => 910,
+  'Descent' => -262,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 365,
+);
+$unitsPerEm=2048;
+$up=-178;
+$ut=119;
+$strp=250;
+$strs=50;
+$ttffile='/Users/hiltamandine/Desktop/sites_php/www/planetecuisine/vendor/mpdf/mpdf/ttfonts/bertoltbrecht.ttf';
+$TTCfontID='0';
+$originalsize=26888;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='bertoltbrecht';
+$panose=' 0 0 2 0 0 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 750, -250, 0
+// usWinAscent/usWinDescent = 910, -281
+// hhea Ascent/Descent/LineGap = 910, -281, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
---
+++ vendor/mpdf/mpdf/ttfontdata/cinzel.mtx.php
@@ -0,0 +1,38 @@
+<?php
+$name='Cinzel-Regular';
+$type='TTF';
+$desc=array (
+  'CapHeight' => 700,
+  'XHeight' => 600,
+  'FontBBox' => '[-11 -239 1328 902]',
+  'Flags' => 4,
+  'Ascent' => 902,
+  'Descent' => -239,
+  'Leading' => 0,
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 365,
+);
+$unitsPerEm=2048;
+$up=-75;
+$ut=50;
+$strp=300;
+$strs=50;
+$ttffile='/Users/hiltamandine/Desktop/sites_php/www/planetecuisine/vendor/mpdf/mpdf/ttfonts/cinzel.ttf';
+$TTCfontID='0';
+$originalsize=58888;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='cinzel';
+$panose=' 0 0 0 0 5 0 0 0 0 0 0 0';
+$haskerninfo=false;
+$haskernGPOS=false;
+$hassmallcapsGSUB=false;
+$fontmetrics='win';
+// TypoAscender/TypoDescender/TypoLineGap = 800, -200, 0
+// usWinAscent/usWinDescent = 914, -258
+// hhea Ascent/Descent/LineGap = 914, -258, 0
+$useOTL=0x0000;
+$rtlPUAstr='';
+?>
