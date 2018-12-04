f38f8690171981e39d6c4d14d327d1ed773910d2
COUE Anthony <a.coue@has-sante.fr>
2018-12-05 00:23:04 +0100

    Fin inscription passages

--- src/Controller/InscriptionPassagesController.php
+++ src/Controller/InscriptionPassagesController.php
@@ -1,12 +1,9 @@
 <?php
-
-
-
 namespace App\Controller;
 
 use Cake\Event\Event;
 use Cake\ORM\TableRegistry;
-use Cake\Http\Response;
+use Cake\I18n\Time;
 
 class InscriptionPassagesController extends AppController
 {
@@ -20,32 +17,28 @@ class InscriptionPassagesController extends AppController
     public function beforeFilter(Event $event)
     {
         $this->Security->config('unlockedFields', ['licencies','inscription_competitions']);
-        $this->Auth->allow(['index',
-                            'inscriptions',
-						    'organisateur',
-						    'export',
-						    'gestion',
-						    'view']);
+        $this->Auth->allow([ 'inscriptions']);
     }
     
     function inscriptions($id) {
-	
-        // On prend les données de l'événement        
-        $this->loadModel('Evenements');
         
+        // On prend les données de l'événement        
+        $this->loadModel('Evenements');        
         $event = $this->Evenements->find()->contain(['Passages'])->first();        
         $this->set('id',$id);
         $title = $event['name'];
         $description = $event['name'];
         
+        $idPassage = $event->passage->id;
+        //on retrouve les infos du passage
+        $this->loadModel('Passages');
+        $passage = $this->Passages->find()->where(['id'=> $idPassage])->first();
         // On envoie article pour que le Form soit lié à la table InscriptionPassages    
-        $passage = $this->InscriptionPassages->newEntity();        
+        //$passage = $this->InscriptionPassages->newEntity();        
         
         // Envoie de l'image, dépend de si y en a une uploadée ou pas        
         if(!empty($event['image'])) $headimg = 'headers/evenements/g-'.$event['image']; 
         else $headimg =  'header_main.png';
-         
-        
         
         // Si l'utilisateur est connecté
         if($this->Auth->User('id') !== null){
@@ -56,35 +49,76 @@ class InscriptionPassagesController extends AppController
             // On cherche les inscriptions à l'événement qu'il a déjà faite            
             $inscriptions = $this->InscriptionPassages->find()
                                                       ->contain(['Licencies'])
-                                                      ->where(['user_id' => $user_id,'passage_id' => $event->passage->id]);
-            
-        } else {
-        
+                                                      ->where(['user_id' => $user_id,'passage_id' => $idPassage]);
+        } else {        
             // Sinon, on met un user_id par défaut pour pas faire bugger la sauvegarde ????? A REPRENDRE
             $user_id = 1;
-       }
-       if($this->request->is(['patch','post','put'])){
-        	
-           $data = $this->request->data;	     
-    
-        // Si c'est un nouveau club, on le rentre dans la base
-        if($data['new_club'] == 1) {
-	
-        	$club = $this->InscriptionPassages->Clubs->newEntity();
-        	$club->name = strtoupper($data['club_name']);
-            $club->region_id = $data['region_id'];
-            $result_club = $this->InscriptionPassages->Clubs->save($club);
-        	
-            $data['licencie']['club_id'] = $result_club['id'];
-        	$data['club_id'] = $result_club['id'];
-	       }        
+        }
+       
+        if($this->request->is(['patch','post','put'])){
+            $data = $this->request->data;	     
+            //debug($data);die();
+            // Si c'est un nouveau club, on le rentre dans la base
+            if($data['new_club'] == 1) {
+                $clubTable = TableRegistry::get('Clubs');
+                $club = $clubTable->newEntity();
+                $club->id = null;
+            	$club->name = strtoupper($data['club_name']);
+                $club->region_id = $data['region_id'];
+                $clubTable->save($club);
+                $data['club_id'] = $club->id;
+            }    
+            
+            $nbInscription = count($data['licence']);
+        
+            for ($i = 0; $i < $nbInscription; $i++) {
+                //On retrouve le licencié si existant
+                $this->loadModel('Licencies');
+                $licencie = $this->Licencies->find()
+                                            ->select(['id'])
+                                            ->where(['numero_licence' => $data['licence'][$i], 'nom' => $data['nom'][$i], 'prenom' => $data['prenom'][$i], 'sexe' => $data['sexe'][$i], 'grade_id' => $data['grade'][$i]])
+                                            ->first();
+                //debug($licencie);die();
+                if($licencie) {
+                    $licencie_id = $licencie->id;
+                } else {
+
+                    $licencieTable = TableRegistry::get('Licencies');
+                    $newLicencie = $licencieTable->newEntity();
+                    $newLicencie->id = null;
+                    $newLicencie->numero_licence = strtoupper($data['licence'][$i]);
+                    $newLicencie->nom = strtoupper($data['nom'][$i]);
+                    $newLicencie->prenom = $data['prenom'][$i];
+                    $newLicencie->ddn = Time::createFromFormat('d/m/Y', $data['ddn'][$i])->format('Y');
+                    $newLicencie->sexe = $data['sexe'][$i];
+                    $newLicencie->grade_id = $data['grade'][$i];
+                    $newLicencie->club_id = $data['club_id'];
+                    $newLicencie->discipline_id = $passage->discipline_id;                    
+                    $licencieTable->save($newLicencie);
+                    $licencie_id = $newLicencie->id;;
+                }
+             
+                $inscriptionTable = TableRegistry::get('InscriptionPassages');
+                $inscription = $inscriptionTable->newEntity();
+                $inscription->id = null;
+                $inscription->club_id = $data['club_id'];
+                $inscription->passage_id = $idPassage;
+                $inscription->user_id = $user_id;
+                $inscription->grade_presente_id = $data['grade_presente'][$i];
+                $inscription->commentaire = $data['commentaire'];
+                $inscription->licencie_id = $licencie_id;
+                //debug($inscription);
+                $inscriptionTable->save($inscription);
+            }	       
 		}   
 		
 		// On envoie les variables pour les listes déroulantes et les catégories pour le jQuery
-		$this->set('clubs', $this->InscriptionPassages->Clubs->find('list')->order(['name'=>'ASC']));
-		$this->set('regions', $this->InscriptionPassages->Regions->find('list')->order(['name'=>'ASC']));
-		$this->set('grades', $this->InscriptionPassages->Grades->find('list')->order(['id'=>'ASC']));
-		$this->set(compact(['passage', 'title', 'description', 'headimg', 'event', 'article', 'inscriptions']));
+		$clubs = $this->InscriptionPassages->Clubs->find('list')->order(['name'=>'ASC']);
+		$this->loadModel("Regions");
+		$regions = $this->Regions->find('list')->order(['name'=>'ASC']);
+		$this->loadModel("Grades");
+		$grades = $this->Grades->find('list', ['keyField' => 'id', 'valueField' => 'name'])->order(['id'=>'ASC']);
+		$this->set(compact(['passage', 'title', 'description', 'headimg', 'event', 'article', 'inscriptions','clubs','regions','grades']));
 		
     }	
 	
---
+++ tests/Fixture/InscriptionPassagesFixture.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * InscriptionPassagesFixture
+ *
+ */
+class InscriptionPassagesFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'commentaire' => ['type' => 'text', 'length' => null, 'null' => true, 'default' => null, 'collate' => 'utf8_bin', 'comment' => '', 'precision' => null],
+        'passage_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'licencie_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'grade_presente_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'user_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'club_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_indexes' => [
+            'passage_inscripyion_fk_idx' => ['type' => 'index', 'columns' => ['passage_id'], 'length' => []],
+            'licencie_inscription_fk_idx' => ['type' => 'index', 'columns' => ['licencie_id'], 'length' => []],
+            'grade_inscription_fk_idx' => ['type' => 'index', 'columns' => ['grade_presente_id'], 'length' => []],
+            'user_inscription_fk_idx' => ['type' => 'index', 'columns' => ['user_id'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_bin'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'commentaire' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'passage_id' => 1,
+            'licencie_id' => 1,
+            'grade_presente_id' => 1,
+            'user_id' => 1,
+            'created' => '2018-12-04 10:57:18',
+            'modified' => '2018-12-04 10:57:18',
+            'club_id' => 1
+        ],
+    ];
+}
