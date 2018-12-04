1e52e59f3daac7aaa6dadb17a57669692893b91f
Ahwidemedia <44965367+Ahwidemedia@users.noreply.github.com>
2018-12-04 14:04:52 +0100

    Modifs Christiane

--- src/Controller/InscriptionCompetitionsController.php
+++ src/Controller/InscriptionCompetitionsController.php
@@ -38,7 +38,6 @@ class InscriptionCompetitionsController extends AppController
         $this->Security->config('unlockedFields', ['licencies','inscription_competitions']);
  
         $this->Auth->allow(['index',
-                            'inscriptions',
 						    'organisateur',
 						   'export',
 						   'gestion',
@@ -544,7 +543,7 @@ class InscriptionCompetitionsController extends AppController
             
             if(!empty($dataa['licencie']['nom'])){
                
-             
+           
               
                  // Je vérifie s'il existe ou pas
                         $licencio = $this->InscriptionCompetitions->Licencies->find('all')
@@ -803,8 +802,10 @@ class InscriptionCompetitionsController extends AppController
     
     
     
-	// la variable category détermine si on est sur une catégorie spé ou sur le général
-	public function organisateur($id,$category=null)
+	// la variable category détermine si on est sur une catégorie spécifique ou sur le général
+	
+    
+    public function organisateur($id,$category=null)
     {
 		
         $this->loadModel('Evenements');
@@ -821,7 +822,8 @@ class InscriptionCompetitionsController extends AppController
         
         $this->set('id',$id);
       
-         
+        $user_id =  $this->Auth->User('id');
+        
          $title = $event['name'];
          $description = $event['name'];
         
@@ -853,19 +855,88 @@ class InscriptionCompetitionsController extends AppController
         return $q->contain(['Clubs','Grades']);
     }]);
 		
+            
+            
+
+            
 		
        // Sinon, on prend que les inscros de la caté qui nous intéresse
         
         }
         
        else {
+           
+           
+              
+                //Si on est dans les excellences femmes
+		if($category == 7) {
 			
+            // On prend les espoirs (id = 3) en plus des femmes excellence(id =7)
+             
+              $articles = $this->InscriptionCompetitions->find('all')
+        ->where(['category_id'=>$category, 'competition_id'=>$event['competition']['id'],'participation_indiv'=>'1'])
+        ->orWhere(['competition_id'=>$event['competition']['id'],'category_id'=>'3','surclassement_age'=>'1','participation_indiv'=>'1'])
+        ->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+			 }]);
+            
+      
+			
+        }
+		
+
+           
+           //Si on est dans les honneurs hommes
+		elseif($category == 10) {
+			
+            // On prend les juniors non surclassés en grade (id=5) et les honneurs hommes (id = 10)
+            
+			  
+              $articles = $this->InscriptionCompetitions->find('all')
+         ->where(['category_id'=>$category,'surclassement_grade'=>'0', 'competition_id'=>$event['competition']['id'],'participation_indiv'=>'1'])
+        ->orWhere(['category_id'=>'5','surclassement_age'=>'1','surclassement_grade'=>'0','participation_indiv'=>1])
+        ->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+			 }]);
+            
+           
+            
+		}
+           
+        
+		
+		// Si on est dans les excellences hommes
+		elseif($category == 11) {
+			
+             
+            // On prend les juniors surclassés en grade et age (id=5), les honneurs hommes surclassés (id=10) et les excellences hommes (id=11)
+    
+            	  
+              $articles = $this->InscriptionCompetitions->find('all')
+         ->where(['category_id'=>$category, 'competition_id'=>$event['competition']['id'],'participation_indiv'=>'1'])
+        ->orWhere([ 'competition_id'=>$event['competition']['id'],'surclassement_grade'=>'1','participation_indiv'=>'1'])
+        
+        ->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+			 }]);
+          
+            
+		}
+           
+           // Si c'est une caté sans surclassement possible
+
+           else {
+           
+           
 		    $articles = $this->InscriptionCompetitions->find('all')
          ->where(['category_id'=>$category, 'competition_id'=>$event['competition']['id'],'participation_indiv'=>'1'])
 			->contain(['Categories','Licencies'=> function($q) {
         return $q->contain(['Clubs','Grades']);
 			 }]);
-			
+				$this->set('articles',$articles);
+               }
+               
+               
            // On prend également les données de la catégorie présente
            
           $category_info = $this->InscriptionCompetitions->Categories->find('all')
@@ -874,166 +945,151 @@ class InscriptionCompetitionsController extends AppController
 		
            $this->set('category_info',$category_info);
                        
-                       }
+           
+        	
+                      
+       }
 		
-			
-		$this->set('articles',$articles);
+			$this->set('articles',$articles);   
+	
  
+        
     }
 	
 	
 	
 	// Fonction csv à partir du plug in
-	public function export($category=null) {
+	public function export($id,$category=null) {
 		
+         $user_id =  $this->Auth->User('id');
+        
 		$this->response->download('export.csv');
 	
-	
-	// Former le le nom de la catégorie
-		 if($category !== null) {
-	
-	if($category == 13) {$name = 'Catégorie -Poussins';}
-	if($category == 1) {$name = 'Catégorie -Samourais';}
-	if($category == 2) {$name = 'Catégorie -Benjamins';}
-	if($category == 3) {$name = 'Catégorie -Minimes';}
-	if($category == 4) {$name = 'Catégorie -Cadets';}
-	if($category == 5) {$name = 'Catégorie -Juniors';}
-	if($category == 6) {$name = 'Catégorie -Espoirs';}
-	if($category == 7) {$name = 'Catégorie -Honneurs';}
-	if($category == 8) {$name = 'Catégorie -Excellences';}
-	if($category == 9) {$name = 'Catégorie -Kyusha';}
-	if($category == 10) {$name = 'Catégorie -Femmes';}
-		} else {
 		
-		$name = 'Toutes les catégories';
-		
-		}
-		
-		// Junior + Junior & Honneurs + Junior & Excellence
-		if($category == 6) {
-			
-			$category = array('11','6','12');
-		}
-		
-		
-		// Junior + Junior & Honneurs 
-		if($category == 7) {
-			
-			$category = array('11','7');
-		}
-		
-		
-		// Excellence + Junior & Excellence 
-		if($category == 8) {
-			
-			$category = array('12','8');
-		}
-		
-		
-		// Espoir + Espoir & Femme 
-		if($category == 5) {
-			
-			$category = array('14','5');
-		}
-		
-		
-		// Femmes + Espoir & Femme 
-		if($category == 10) {
-			
-			$category = array('14','10');
-		}
-		
-		
-		
-		// Si on a pas de catégories, on prend tout
+ 	// Si on n'a pas de catégorie spécifiée, on prend tout
 		if($category == null) {
 		
-			$data = $this->Inscriptions->Inscrits->find('all')
-			->contain('Categories','Clubs')
-    		->toArray();
+            $data = $this->InscriptionCompetitions->find('all')
+         ->where(['competition_id'=>$id,'participation_indiv'=>'1'])
+			->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+    }]);
 		
-			 
+            
+            
+            $name = 'Tous';
+            
+		
+       // Sinon, on prend que les inscros de la caté qui nous intéresse
+        
+        }
+        
+       else {
+           
+           // Pour avoir le nom de la catégorie
+           
+          $cate = $this->InscriptionCompetitions->Categories->find('all')
+               ->where(['id'=>$category])
+               ->first();
+           
+           $name = $cate['name'];
+           
+           
+                //Si on est dans les excellences femmes
+		if($category == 7) {
+			
+            // On prend les espoirs (id = 3) en plus des femmes excellence(id =7)
+             
+              $data = $this->InscriptionCompetitions->find('all')
+        ->where(['category_id'=>$category, 'competition_id'=>$id,'participation_indiv'=>'1'])
+        ->orWhere(['competition_id'=>$id,'category_id'=>'3','surclassement_age'=>'1','participation_indiv'=>'1'])
+        ->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+			 }]);
+            
+      
+			
+        }
+		
+
+           
+           //Si on est dans les honneurs hommes
+		elseif($category == 10) {
+			
+            // On prend les juniors non surclassés en grade (id=5) et les honneurs hommes (id = 10)
+            
+			  
+              $data = $this->InscriptionCompetitions->find('all')
+         ->where(['category_id'=>$category, 'surclassement_grade'=>'0','competition_id'=>$id,'participation_indiv'=>'1'])
+        ->orWhere(['category_id'=>'5','surclassement_age'=>'1','surclassement_grade'=>'0','participation_indiv'=>1])
+        ->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+			 }]);
+            
+           
+            
 		}
+           
+        
 		
-		// Si on a 2 catégories (surclassement, combat dans les deux catés)
-		
-			elseif(isset($category[1])) {
+		// Si on est dans les excellences hommes
+		elseif($category == 11) {
 			
-			$data = $this->Inscriptions->Inscrits->find('all')
-			->contain('Categories','Clubs')
-			->where(['Inscrits.category_id IN'=>$category])
-			->toArray();	
-	
-			
+             
+            // On prend les juniors surclassés en grade et age (id=5), les honneurs hommes surclassés (id=10) et les excellences hommes (id=11)
+    
+            	  
+              $data = $this->InscriptionCompetitions->find('all')
+         ->where(['category_id'=>$category, 'competition_id'=>$id,'participation_indiv'=>'1'])
+        ->orWhere([ 'competition_id'=>$id,'surclassement_grade'=>'1','participation_indiv'=>'1'])
+        ->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+			 }]);
+          
+            
 		}
-		else {
-			
-			
-			// Si cela ne correspond qu'à une seule catégorie
-			$data = $this->Inscriptions->Inscrits->find('all')
-			->contain('Categories')
-			->contain('Clubs')	
-			->where(['Inscrits.category_id'=>$category])
-			->toArray();
-	}
-			
-		
-		
-		
-		foreach($data as $datas) {
-			
-			$datas['category_id'] = $datas['category']['name'];
-			$datas['club_id'] = $datas['club']['name'];
-			
-			// On retranscrit les grades en nom pour le tableau
-	if($datas['grade'] == -10) {$datas['grade'] = '10ème Kyu';}
-			elseif($datas['grade'] == -10) {$datas['grade'] = '10eme Kyu';}
-			elseif($datas['grade'] == -9) {$datas['grade'] = '9eme Kyu';}
-			elseif($datas['grade'] == -8) {$datas['grade'] = '8eme Kyu';}
-			elseif($datas['grade'] == -7) {$datas['grade'] = '7eme Kyu';}
-			elseif($datas['grade'] == -6) {$datas['grade'] = '6eme Kyu';}
-			elseif($datas['grade'] == -5) {$datas['grade'] = '5eme Kyu';}
-			elseif($datas['grade'] == -4) {$datas['grade'] = '4eme Kyu';}
-			elseif($datas['grade'] == -3) {$datas['grade'] = '3eme Kyu';}
-			elseif($datas['grade'] == -2) {$datas['grade'] = '2eme Kyu';}
-			elseif($datas['grade'] == -1) {$datas['grade'] = '1er Kyu';}
-			elseif($datas['grade'] == 1) {$datas['grade'] = '1er Dan';}
-			elseif($datas['grade'] == 2) {$datas['grade'] = '2eme Dan';}
-			elseif($datas['grade'] == 3) {$datas['grade'] = '3eme Dan';}
-			elseif($datas['grade'] == 4) {$datas['grade'] = '4eme Dan';}
-			elseif($datas['grade'] == 5) {$datas['grade'] = '5eme Dan';}
-			elseif($datas['grade'] == 6) {$datas['grade'] = '6eme Dan';}
-			elseif($datas['grade'] == 7) {$datas['grade'] = '7eme Dan';}
-	
-			
-	// On retransmet le nom des catégories à partir de l'id
-	if($datas['category_id'] == 13) {$datas['category_id'] = 'Poussins';}
-	if($datas['category_id'] == 1) {$datas['category_id'] = 'Samourais';}
-	if($datas['category_id'] == 2) {$datas['category_id'] = 'Benjamins';}
-	if($datas['category_id'] == 3) {$datas['category_id'] = 'Minimes';}
-	if($datas['category_id'] == 4) {$datas['category_id'] = 'Cadets';}
-	if($datas['category_id'] == 5) {$datas['category_id'] = 'Juniors';}
-	if($datas['category_id'] == 6) {$datas['category_id'] = 'Espoirs';}
-	if($datas['category_id'] == 7) {$datas['category_id'] = 'Honneurs';}
-	if($datas['category_id'] == 8) {$datas['category_id'] = 'Excellences';}
-	if($datas['category_id'] == 9) {$datas['category_id'] = 'Kyusha';}
-	if($datas['category_id'] == 10) {$datas['category_id'] = 'Femmes';}
-			
-			
-			// On va écrire s'il y a surclassement
-			if($datas['surclassement_age'] == 1 OR $datas['surclassement_grade']== 1) {
+           
+           // Si c'est une caté sans surclassement possible
+
+           else {
+           
+           
+		    $data = $this->InscriptionCompetitions->find('all')
+         ->where(['category_id'=>$category, 'competition_id'=>$id,'participation_indiv'=>'1'])
+			->contain(['Categories','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+			 }]);
 				
-				$datas['surclasse'] = 'Oui';
-			}else {
-				
-				$datas['surclasse'] = 'Non';
-				
-			}
+               }
+                     
+       }
 		
-		
-		}
-		
-		
+        
+		foreach($data as $datas){
+            
+         
+           
+            $datas['nom'] = $datas['licency']['nom'];
+            $datas['prenom'] = $datas['licency']['prenom'];
+            $datas['sexe'] = $datas['licency']['sexe'];
+            $datas['ddn'] = $datas['licency']['ddn'];
+            $datas['category'] = $datas['category']['name'];
+            $datas['grade'] = str_replace('è','e',$datas['licency']['grade']['name']);
+            $datas['club'] = $datas['licency']['club']['name'];
+            
+            if($datas['surclassement_grade']== 1 OR $datas['surclassement_age'] == 1) {
+                
+                $datas['surclasse'] = 'X';
+                
+            }else {$datas['surclasse'] = '';}
+            
+           
+        }
+        
+       
+        
+       
+        
 		
 		// Données à envoyer au plugin
 		 $_serialize = 'data';
@@ -1042,7 +1098,7 @@ class InscriptionCompetitionsController extends AppController
     	 $_delimiter = ';'; 
     	 
     	 // colonnes sélectionnées
-    	 $_extract = array( 'nom', 'prenom','sexe','grade','age','category_id','surclasse','club_id');
+    	 $_extract = array( 'nom', 'prenom','sexe','grade','ddn','category_id','surclasse','club');
 		
 		// Nom des colonnes
 		 $_header = [ 'Nom', 'Prenom','Sexe','Grade','Annee de naissance','Categorie','Surclasse','club'];
@@ -1056,25 +1112,28 @@ class InscriptionCompetitionsController extends AppController
 	
 	
 	
+    
+    
 	// Fonction pour contrôler les inscriptions
-	public function gestion($id) {
+	public function equipes($id) {
         
         
-          
-        $this->loadModel('Evenements');
+            $this->loadModel('Evenements');
         
         $event = $this->Evenements->find()
 	->contain(['Competitions'=> function(\Cake\ORM\Query $q) use ($id) {
-        return $q->where(['Competitions.id' => $id]);
+        return $q->where(['Competitions.id' => $id])->contain(['Categories']);
     }])
 
 	->matching('Competitions', function(\Cake\ORM\Query $q) use($id) {
-        return $q->where(['Competitions.id' => $id]);
+        return $q->where(['Competitions.id' => $id])->contain(['Categories'=> function($q) {
+        return $q->order(['annee_debut'=>'DESC','grade_debut'=>'DESC']);}]);
     })->first();
         
         $this->set('id',$id);
       
-         
+        $user_id =  $this->Auth->User('id');
+        
          $title = $event['name'];
          $description = $event['name'];
         
@@ -1091,17 +1150,94 @@ class InscriptionCompetitionsController extends AppController
           $this->set('event',$event);
         
         
-        
-      $user_id =  $this->Auth->User('id');
-	
-	$articles =	$this->InscriptionCompetitions->find('all')
-	->where(['competition_id'=>$id, 'user_id'=>$user_id]);
+       
 
-	
+		
+            $articles = $this->InscriptionCompetitions->find('all')
+         ->where(['InscriptionCompetitions.competition_id'=>$id,'participation_equipe'=>'1'])
+                ->order(['equipe_id'=>'ASC'])
+			->contain(['Equipes','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+    }]);
+		
+            
+            
 	$this->set('articles',$articles);
 	
 	}
+    
+    
+    
+    
+    	// Fonction csv à partir du plug in
+	public function exportequipe($id) {
+		
+         $user_id =  $this->Auth->User('id');
+        
+		$this->response->download('export.csv');
 	
+        $name = 'Equipes';
+		
+ 	  $data = $this->InscriptionCompetitions->find('all')
+         ->where(['InscriptionCompetitions.competition_id'=>$id,'participation_equipe'=>'1'])
+                ->order(['equipe_id'=>'ASC'])
+			->contain(['Equipes','Licencies'=> function($q) {
+        return $q->contain(['Clubs','Grades']);
+    }]);
+		
+		
+        
+		foreach($data as $datas){
+            
+         
+           
+            $datas['nom'] = $datas['licency']['nom'];
+            $datas['prenom'] = $datas['licency']['prenom'];
+            $datas['sexe'] = $datas['licency']['sexe'];
+            $datas['ddn'] = $datas['licency']['ddn'];
+            $datas['grade'] = str_replace('è','e',$datas['licency']['grade']['name']);
+            $datas['club'] = $datas['licency']['club']['name'];
+            $datas['equipe'] = strtoupper($datas['equipe']['name']);
+            
+            if($datas['surclassement_grade']== 1 OR $datas['surclassement_age'] == 1) {
+                
+                $datas['surclasse'] = 'X';
+                
+            }else {$datas['surclasse'] = '';}
+            
+           
+        }
+        
+       
+        
+       
+        
+		
+		// Données à envoyer au plugin
+		 $_serialize = 'data';
+		 
+		 // Choix du délimitant du csv pour lecture correcte en excel
+    	 $_delimiter = ';'; 
+    	 
+    	 // colonnes sélectionnées
+    	 $_extract = array( 'equipe','nom', 'prenom','sexe','grade','ddn','surclasse','club');
+		
+		// Nom des colonnes
+		 $_header = [ 'Equipe','Nom', 'Prenom','Sexe','Grade','Annee de naissance','Surclasse','club'];
+		
+		$this->set(compact('data', '_serialize', '_extract', '_delimiter','_header'));		
+		$this->viewBuilder()->className('CsvView.Csv');
+		
+		$this->response->download($name.'.csv');
+	}
+
+	
+	
+	
+    
+	
+    
+    
 	
 	// Fonction pdf, marche comme la csv
 	
